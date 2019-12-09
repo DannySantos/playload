070ab1aa5f727815d55573fb713661a78dc47d6b
Danny Santos <dannyangelosanto@gmail.com>
2019-12-09 15:09:21 +0000

    Adds find_by method to user repo

--- spec/playload/repositories/user_repository_spec.rb
+++ spec/playload/repositories/user_repository_spec.rb
@@ -1,3 +1,23 @@
 RSpec.describe UserRepository, type: :repository do
-  # place your tests here
+  describe '#find_by' do
+    let(:result) { subject.find_by(email: find_by_email) }
+    let(:email)  { Faker::Internet.email }
+    let!(:user)  { create(:user, email: email) }
+    
+    context 'when the email address matches the user' do
+      let(:find_by_email) { email }
+
+      it 'returns the user' do
+        expect(result).to eq(user)
+      end
+    end
+    
+    context 'when the email address does not match the user' do
+      let(:find_by_email) { Faker::Internet.email }
+
+      it 'returns nil' do
+        expect(result).to eq(nil)
+      end
+    end
+  end
 end

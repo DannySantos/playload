cf499f8d7bcb156900a0c47b4cf2f22ec7d4dfa4
Danny Santos <dannyangelosanto@gmail.com>
2020-01-21 21:41:47 +0000

    Adds styling   - Adds SASS   - Adds Simple Grid   - Adds Normalize   - Adds The One Framework

--- apps/web/views/sessions/new.rb
+++ apps/web/views/sessions/new.rb
@@ -8,11 +8,19 @@ module Web
 
         def log_in_form
           form_for :session, routes.sessions_path do
-            label :email
-            email_field :email
-            label :password
-            password_field :password
-            submit 'Log in'
+            div class: 'col-1-1 mb10' do
+              label :email
+              email_field :email, class: 'w100p'
+            end
+
+            div class: 'col-1-1 mb10' do
+              label :password
+              password_field :password, class: 'w100p'
+            end
+
+            div class: 'col-1-1 mb10 tac' do
+              submit 'Log in'
+            end
           end
         end
       end

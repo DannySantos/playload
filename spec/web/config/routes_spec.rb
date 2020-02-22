4ec5da848f6bcc8da70e88409e351fdf1ec5f2a3
David Swift <davidswift@Davids-MacBook-Pro.local>
2020-02-22 11:42:18 +0000

    Move test variables into let blocks for readability

--- spec/web/config/routes_spec.rb
+++ spec/web/config/routes_spec.rb
@@ -2,16 +2,15 @@
 
 RSpec.describe Web.routes do
   describe 'DELETE session' do
-    it 'has a generated path' do
-      generated_path = described_class.path(:logout)
+    let(:generated_path) { described_class.path(:logout) }
+    let(:env) { Rack::MockRequest.env_for('/sessions', method: 'DELETE') }
+    let(:route) { described_class.recognize(env) }
 
+    it 'has a generated path' do
       expect(generated_path).to eq('/sessions')
     end
 
     it 'is a recognisable route' do
-      env = Rack::MockRequest.env_for('/sessions', method: 'DELETE')
-      route = described_class.recognize(env)
-
       expect(route).to be_routable
       expect(route.path).to eq('/sessions')
       expect(route.verb).to eq('DELETE')
